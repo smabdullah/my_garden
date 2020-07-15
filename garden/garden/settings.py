4fcc9bcae741510d79c7598371524bee5edb7ac0
SM Abdullah <sma.csedu@gmail.com>
2020-07-15 22:41:41 +1000

    remove the secret key and set it as an environment variable

--- garden/garden/settings.py
+++ garden/garden/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9_q!w&ctrovmm)j$=us$8o8@*j^x%!^zx*a172jn_%o(1^2!bc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
