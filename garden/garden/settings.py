5d9c3325908f5a04e68fe100e63e7f90c766f910
SM Abdullah <sma.csedu@gmail.com>
2020-07-16 21:04:06 +1000

    updated login page

--- garden/garden/settings.py
+++ garden/garden/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 #login redirect URL
 LOGIN_REDIRECT_URL = 'home'
